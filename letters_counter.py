6d3418431eed004ee86c808427c7277c6cfe5d27
Valerio Caporioni <126289423+tuonopettinato@users.noreply.github.com>
2023-09-27 15:52:05 +0200

    updated

--- letters_counter.py
+++ letters_counter.py
@@ -15,32 +15,35 @@ parser = argparse.ArgumentParser(prog='wordcount',
 parser.add_argument('infile')
 parser.add_argument('histogram_choice')
 
-
-
 def process_file(infile, histogram_choice):
     """
     """
     logger.info(f'Opening input file {infile}...')
     data = open(infile, encoding= 'utf-8').read()
-    print(len(data))
-    logger.info(f'Done, {len(data)} character(s) found.')
+    total_characters = len(data)
+    print(total_characters)
+    logger.info(f'Done, {total_characters} character(s) found.')
     histogram = np.array([])
     for letter,cap_letter in zip(alphabet, cap_alphabet):
-        letter_occurrence = data.count(letter) + data.count(cap_letter)
-        histogram = np.append(histogram, letter_occurrence)
-        print(cap_letter,':  ', letter_occurrence)
-    plt.figure('Letter occurrences')
-    plt.bar(alphabet, histogram/np.sum(histogram), width=0.5, color='tab:blue')
-    end_time = time.time()
-    runtime = end_time - start_time
-    logger.info(f'Runtime is: {runtime:.2f} seconds')
+        letter_frequency = 100*(data.count(letter) + data.count(cap_letter))/total_characters
+        histogram = np.append(histogram, letter_frequency)
+        print(cap_letter, ':',  letter_frequency, '%' )
+    ordering = np.argsort(histogram)[::-1]
+    histogram = np.sort(histogram)[::-1]
+    label_letters = [alphabet[i] for i in ordering]
     if histogram_choice  == 'y':
+        plt.figure(f'Letter frequency in {infile}')
+        plt.bar(label_letters, histogram, width=0.5, color='tab:blue')
+        plt.ylabel('Frequency (%)')
+        end_time = time.time()
+        runtime = end_time - start_time
+        logger.info(f'Runtime is: {runtime:.2f} seconds')
         plt.show()
     else: None
 
 if __name__== '__main__':
-args = parser.parse_args()
-process_file(args.infile, args.histogram_choice)
+    args = parser.parse_args()
+    process_file(args.infile, args.histogram_choice)
 
 
 
